d4c52decc3fb9dd90b88e75a810bbe2ce6c113ad
Alex Rafael Oneda <alexoneda@live.com>
2024-02-26 21:53:35 -0300

    Aula 26-02-24

---
+++ Atividades/Ficha Cadastral/Program.cs
@@ -0,0 +1,49 @@
+﻿string nomeUsuario, eMailUsuario, generoUsuario, cepUsuario, ruaUsuario, numeroUsuario, bairroUsuario, cidadeUsuario, ufUsuario, paisUsuario, dataNascimento = string.Empty;
+Console.WriteLine(@"Seja bem vindo ao sistema de ficha cadastral.
+        Para iniciar, digite seu nome:
+    ");
+nomeUsuario = Console.ReadLine();
+
+Console.WriteLine("Digite seu e-Mail:");
+eMailUsuario = Console.ReadLine();
+
+Console.WriteLine("Digite sua data de nascimento (dd/mm/yy)");
+dataNascimento = Console.ReadLine();
+
+Console.WriteLine("Digite seu gênero:");
+generoUsuario = Console.ReadLine();
+
+Console.WriteLine("Digite seu CEP:");
+cepUsuario = Console.ReadLine();
+
+Console.WriteLine("Digite o nome da sua rua:");
+ruaUsuario = Console.ReadLine();
+
+Console.WriteLine("Digite o número da sua casa:");
+numeroUsuario = Console.ReadLine();
+
+Console.WriteLine("Digite o nome do seu bairro:");
+bairroUsuario = Console.ReadLine();
+
+Console.WriteLine("Digite o nome da sua cidade:");
+cidadeUsuario = Console.ReadLine();
+
+Console.WriteLine("Digite seu estado:");
+ufUsuario = Console.ReadLine();
+
+Console.WriteLine("Digite o nome do seu país:");
+paisUsuario = Console.ReadLine();
+
+Console.WriteLine(@$"Seus dados:
+    [NOME]:         {nomeUsuario}
+    [EMAIL]:        {eMailUsuario}
+    [NASCIMENTO]:   {dataNascimento}
+    [GÊNERO]:       {generoUsuario}
+    [CEP]:          {cepUsuario}
+    [RUA]:          {ruaUsuario}
+    [NÚMERO]:       {numeroUsuario}
+    [BAIRRO]:       {bairroUsuario}
+    [CIDADE]:       {cidadeUsuario}
+    [ESTADO]:       {ufUsuario}
+    [PAÍS]:         {paisUsuario}
+");

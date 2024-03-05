6663dbff14cb80af95539eb770e43a34b7596bb5
Alex Rafael Oneda <alexoneda@live.com>
2024-03-04 21:43:46 -0300

    Atividade 240304

---
+++ Atividades/Atividade_240304/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.WriteLine(@$"Selecione o operador aritmético
+* : Multiplicação
+/ : Divisão
+- : Subtração
++ : Adição
+[Qual sua seleção?]");
+
+string? userSelection = Console.ReadLine();
+
+Console.WriteLine("Digite o primeiro número:");
+float firstNumber = float.Parse(Console.ReadLine());
+Console.WriteLine("Digite o segundo número");
+float secondNumber = float.Parse(Console.ReadLine());
+
+Console.WriteLine($"{firstNumber} {secondNumber}");
+
+float finalResult;
+
+switch(userSelection)
+{
+    case "*":
+        finalResult = firstNumber * secondNumber;
+        Console.WriteLine($"A multiplicação de {firstNumber} por {secondNumber} é: {finalResult}");
+        break;
+    case "/":
+        finalResult = firstNumber / secondNumber;
+        Console.WriteLine($"A divisão de {firstNumber} por {secondNumber} é: {finalResult}");
+        break;
+    case "-":
+        finalResult = firstNumber - secondNumber;
+        Console.WriteLine($"A subtração de {firstNumber} por {secondNumber} é: {finalResult}");
+        break;
+    case "+":
+        finalResult = firstNumber + secondNumber;
+        Console.WriteLine($"A adição de {firstNumber} por {secondNumber} é: {finalResult}");
+        break;
+}
